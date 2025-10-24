7d1a0a3c598a9da9cc1fb7c6c22c0978b5b7d0cf
cHavva-hub <me25b063@smail.iitm.ac.in>
2025-10-24 21:37:36 +0530

    Create IBOT-DFS-DC_app.py
    # Krishiv Gupta
    # ME25B063
    # 24 - 10 - 2025

---
+++ IBOT-DFS-DC_app.py
@@ -0,0 +1,27 @@
+# Krishiv Gupta
+# ME25B063
+# 24 - 10 - 2025
+
+
+class Solution:
+    
+    #Function to return a list containing the DFS traversal of the graph.
+    def dfs(self, adj):
+        # code here
+        V = len(adj)
+        visited = [False] * V
+        
+        src = 0
+        res = []
+        visited[src] = True
+        res.append(src)
+        
+        def dfs2(node):
+            curr = adj[node]
+            for x in curr:
+                if not visited[x]:
+                    res.append(x)
+                    visited[x] = True
+                    dfs(x)
+        dfs2(src)
+        return res
